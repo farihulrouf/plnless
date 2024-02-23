ed8c9c73c5f0b689cc3a875e51c0a348ee676f0d
Farihul Rouf <farihulrouf@gmail.com>
2024-02-23 17:53:25 +0700

    exp

--- routes/users.js
+++ routes/users.js
@@ -95,7 +95,7 @@ router.post("/login", async (req, res) => {
 
       process.env.JWT_SECRET,
       {
-        expiresIn: JWT_EXPIRES_IN,
+        expiresIn: process.env.JWT_EXPIRES_IN,
       }
     );
 
